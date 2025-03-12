f0fcc0d248b4d799ccabda7e6cada75b38d43b94
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-03-12 19:15:43 +0300

    refactor: refine product price display

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -6,6 +6,7 @@ import {
   CardFooter,
   CardHeader,
 } from '@/components/ui/card';
+import ProductPrice from './product-price';
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -29,13 +30,12 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">${product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out Of Stock</p>
           )}
         </div>
       </CardContent>
-      <CardFooter>footer</CardFooter>
     </Card>
   );
 };
