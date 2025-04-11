d5af38e9fd604c4a00009bd706c447507baaab7b
Liudmila Rodzina <liudmilarodzina@gmail.com>
2025-04-11 13:00:23 +0300

    feat: create order and orderItem zod schemas and types

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,8 @@ import {
   cartItemSchema,
   shippingAddressSchema,
   paymentMethodSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
 } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -19,4 +21,17 @@ export type CartItem = z.infer<typeof cartItemSchema>;
 
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
-export type PaymentMethod = z.infer<typeof paymentMethodSchema>
+export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+};
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
